6db10b61049723253857f8d618a59e81dfb93a43
RobertoVazquez7 <tlfcrazzyd4rk@gmail.com>
2023-01-05 22:39:10 -0600

    Consumiendo Api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,99 @@
+// import './App.css';
+// import Buscador from './componentes/Buscador';
+// import Resultados from './componentes/Resultado';
+// import React, { Component, useEffect, useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import axios from 'axios';
+
+// class App extends Component {
+
+//   state = {
+//     termino: '',
+//     imagenes: [],
+//     pagina: ''
+//   }
+
+
+//   scroll = () => {
+//     const elemento = document.querySelector('.jumbotron');
+//     elemento.scrollIntoView('smooth', 'start');
+//   }
+
+//   paginaAnterior = () => {
+//     // leer el state de la pagina actual
+//     let pagina = this.state.pagina;
+//     // si la pagina es 1, ya no regresar
+//     if (pagina === 1) return null;
+//     // restarle uno a la pagina actual
+//     pagina -= 1;
+//     // agregar cambio al state
+//     this.setState({
+//       pagina
+//     }, () => {
+//       this.consultarApi();
+//       this.scroll();
+//     });
+//   }
+
+//   paginaSiguiente = () => {
+//     // leer el state de la pagina actual
+//     let pagina = this.state.pagina;
+//     // sumarle uno a la pagina actual
+//     pagina += 1;
+//     // agregar cambio al state
+//     this.setState({
+//       pagina
+//     }, () => {
+//       this.consultarApi();
+//       this.scroll();
+//     });
+//   }
+
+//   // consultarApi = () => {
+//   //   //const termino = this.state.termino;
+//   //   const pagina = this.state.pagina;
+//   //   const url = `https://pixabay.com/api/?key=32198510-34bfb35095049432fc0401547&q=${this.state.termino}&per_page=30&page=${pagina}`;
+//   //   //const url = `http://localhost:8000/api/productos&q=${this.state.termino}&per_page=5&page=${pagina}`;
+//   //   // console.log(url);
+//   //   fetch(url)
+//   //     .then(respuesta => respuesta.json())
+//   //     .then(resultado => this.setState({ imagenes: resultado.hits }))
+//   // }
+
+//   datosBusqueda = (termino) => {
+//     this.setState({
+//       termino: termino,
+//       pagina: 1
+//     }, () => {
+//       this.consultarApi();
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className='column'>
+//         <div className="contenedor">
+//           <div className="jumbotron">
+//             <p className="m-9 lead text-center titulo">BUSCADOR DE PRODUCTOS</p>
+//             <Buscador
+//               datosBusqueda={this.datosBusqueda}
+//             />
+//           </div>
+//         </div>
+//         <div className='contenedor-resultados'>
+//           <Resultados
+//             imagenes={this.state.imagenes}
+//             paginaAnterior={this.paginaAnterior}
+//             paginaSiguiente={this.paginaSiguiente}
+//           />
+//         </div>
+//       </div>
+//     )
+//   };
+// }
+
+// export default App;
+
 import './App.css';
 import Buscador from './componentes/Buscador';
 import Resultados from './componentes/Resultado';
@@ -43,18 +139,16 @@ class App extends Component {
       pagina
     }, () => {
       this.consultarApi();
-      this.scroll();
     });
   }
 
   consultarApi = () => {
-    //const termino = this.state.termino;
+    console.log("llego aqui");
     const pagina = this.state.pagina;
-    const url = `https://pixabay.com/api/?key=32198510-34bfb35095049432fc0401547&q=${this.state.termino}&per_page=30&page=${pagina}`;
-    //console.log(url);
-    fetch(url)
-      .then(respuesta => respuesta.json())
-      .then(resultado => this.setState({ imagenes: resultado.hits }))
+    fetch(`http://localhost:8000/api/productos`)
+    .then((res) => res.json())
+    //.then((imagenes) => console.log(imagenes));
+    .then(resultado => this.setState({ imagenes: resultado }))
   }
 
   datosBusqueda = (termino) => {
--- src/componentes/Imagen.js
+++ src/componentes/Imagen.js
@@ -9,15 +9,17 @@ const Imagen = (props) => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const { largeImageURL, likes, tags, views } = props.imagen;
+    // const { largeImageURL, likes, tags, views } = props.imagen;
+    const { descripcion, imagen, link, nombre, nombreTienda, precio } = props.imagen;
+    // console.log( nombre, descripcion, largeImageURL, likes, tags, views )
 
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb4 contenedor-card">
             <div className="card">
-                <img src={largeImageURL} alt={tags} className="card-img-top" height="200" width="200" />
+                <img src={imagen} className="card-img-top" height="200" width="200" />
                 <div className="card-body text-center">
-                    <p className="card-text">{likes} Me gusta</p>
-                    <p className="card-text">{views} Vistas</p>
+                    <p className="card-text">{nombre} </p>
+                    <p className="card-text">{precio} </p>
                     <div >
                         <Button variant="primary" onClick={handleShow} style={{ backgroundColor: '#C9571A', border: 'none' }} >
                             Ver detalles
@@ -26,26 +28,21 @@ const Imagen = (props) => {
                             <Modal.Header closeButton className='header'>
                                 <Modal.Title>
                                     <div className='column' >
-                                        <h4>Hyundai HyFlip</h4>
-                                        <h5>Disponible en: Walmart</h5>
+                                        <h4> {nombre} </h4>
+                                        <h5>Disponible en: {nombreTienda}</h5>
+                                        <h6> Precio {precio} </h6>
                                     </div>
                                 </Modal.Title>
                             </Modal.Header>
                             <Modal.Body className='body-modal' >
-                                <img className='imagen-detalles' src={largeImageURL} height="250" width="250" />
+                                <img className='imagen-detalles' src={imagen} height="250" width="250" />
                             </Modal.Body>
                             <Modal.Footer className="text-center footer">
                                 <div className='column' >
                                     <p>
-                                        Hyundai HyFlip tiene un procesador Intel Celeron que permite una conectividad de alta velocidad
-                                        y un rendimiento multitarea de gran capacidad de respuesta y una apariencia elegante. Este portátil
-                                        convertible 2 en 1 tiene un diseño plegable de 360° y una pantalla táctil FHD. Hyundai HyFlip es veloz
-                                        y potente. Además puedes expandir su capacidad de almacenamiento ya que cuenta con una ranura M.2 SSD.
-                                        Entre sus principales características destaca un inicio rápido y transiciones más suaves al pasar del
-                                        modo portátil al modo tableta y viceversa. Esta computadora HyFlip es ideal para el trabajo y entretenimiento,
-                                        cuenta con todo lo necesario para una vida larga. Todos los productos Hyundai tienen 1 año de garantía.
+                                        {descripcion}
                                     </p>
-                                    <Button variant="primary" onClick={handleShow} style={{ backgroundColor: '#C9571A', border: 'none' }} >
+                                    <Button variant="primary" href={link} onClick={handleShow} style={{ backgroundColor: '#C9571A', border: 'none' }} >
                                         Ver en pagina original
                                     </Button>
                                 </div>
